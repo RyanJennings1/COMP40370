1356da25ac26cbeeae49ed7b54f39c46d7707b3b
Ryan Jennings <ryanjennings1010@gmail.com>
2020-10-25 00:28:30 +0000

    Finish questions and add outputs

--- practical_4_CLASSIFICATION1/run.py
+++ practical_4_CLASSIFICATION1/run.py
@@ -10,6 +10,8 @@ import numpy as np
 import pandas as pd
 
 from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import plot_tree
 
 # Create the output directory if it does not exist
 pathlib.Path('./output').mkdir(exist_ok=True)
@@ -18,7 +20,7 @@ print('*' * 20)
 print('Question 1')
 print('*' * 20)
 
-# Load the first csv file of sensor data
+# Load the first csv file of marks data
 df_1 = pd.read_csv('./specs/marks_question1.csv')
 
 print('Plotting midterm exam scores vs final exam scores ...')
@@ -29,6 +31,7 @@ ax.set_ylim([0, 100])
 ax.set_xlabel('Final Score')
 ax.set_ylabel('Midterm Score')
 ax.set_title('Plot of Midterm Exam Scores vs Final Exam Scores')
+plt.show()
 plt.tight_layout()
 print('Writing plot to ./output/marks.png ...')
 plt.savefig('output/marks.png', dpi=199)
@@ -44,19 +47,20 @@ print('Plotting line of best fit with scatter plot to help describe model ...')
 plt.scatter(df_1['midterm'], df_1['final'])
 ax = plt.gca()
 x_vals = np.linspace(0, 100)
-y_vals = lin_reg.intercept_ + lin_reg.coef[0] * x_vals
+y_vals = lin_reg.intercept_ + lin_reg.coef_[0] * x_vals
 plt.plot(x_vals, y_vals, '--')
 ax.set_xlim([0, 100])
 ax.set_ylim([0, 100])
 ax.set_xlabel('Final Score')
 ax.set_ylabel('Midterm Score')
 ax.set_title('Plot of Midterm Exam Scores vs Final Exam Scores')
+plt.show()
 plt.tight_layout()
 print('Writing plot to ./output/marks_line_best_fit.png ...')
 plt.savefig('output/marks_line_best_fit.png', dpi=199)
 
 print('Predicting the final mark for a midterm of 86 for question 1.3 ...')
-predicted_value = lin_reg.predict(np.array([[86]])
+predicted_value = lin_reg.predict(np.array([[86]]))
 print(f'For a midterm exam score the final exam score is predicted to be: {predicted_value}')
 
 print('Done!')
@@ -66,42 +70,51 @@ print('*' * 20)
 print('Question 2')
 print('*' * 20)
 
-# Load the second csv file of DNA data
+# Load the second csv file of borrower data
 df_2 = pd.read_csv('./specs/borrower_question2.csv')
 
-# Filter out id attribute from dataset
-print('Dropping attribute "id"...')
-del df_2['id']
+# Filter out TID attribute from dataset
+print('Dropping attribute "TID"...')
+del df_2['TID']
 
-# Discretize the numeric columns (age, income, children) into 3 equal bins
-# Replace the columns with their discretized equivalents
-for num_key in ['age', 'income', 'children']:
-    df_2[num_key] = pd.cut(df_2[num_key], bins=3)
+print('Formatting our input data for Question 2 to use with our models ...')
+# Change HomeOwner, MaritalStatus and DefaultedBorrower fields from strings to floats
+df_2['HomeOwner'] = df_2['HomeOwner'].apply(lambda x: 1.0 if x == 'Yes' else 0.0)
+df_2['MaritalStatus'] = df_2['MaritalStatus'].map({
+    'Single': 0.0,
+    'Married': 1.0,
+    'Divorced': 2.0
+    })
+df_2['DefaultedBorrower'] = df_2['DefaultedBorrower'].apply(lambda x: 1.0 if x == 'Yes' else 0.0)
 
-# Yes/No values on their own result in illegible results for our output
-# so modify the values to represent the answer plus their field name
-for field in ['married', 'car', 'save_act', 'current_act', 'mortgage', 'pep']:
-    df_2[field] = df_2[field].replace('YES', f'YES_{field}')
-    df_2[field] = df_2[field].replace('NO', f'NO_{field}')
+print('Splitting training and test data ...')
+# Thankfully here we are provided with a field that tells us if
+# a borrower defaulted on their loan to see how the other fields
+# impacted the result
+training_data = df_2.drop('DefaultedBorrower', axis=1)
+test_data = df_2['DefaultedBorrower']
 
-# Use the formatting method from earlier
-df_fp = format_frequent_itemsets(dataframe=df_2)
+print('Creating high decision tree classifier ...')
+clf_high = DecisionTreeClassifier(criterion='entropy',
+                                  min_impurity_decrease=0.5)
+clf_high_model = clf_high.fit(training_data, test_data)
 
-# Use the FP-Growth algorithm to generate frequent itemsets
-fp = fpgrowth(df_fp, min_support=0.2, use_colnames=True)
+plt.figure()
+plot_tree(clf_high_model)
+plt.show()
+plt.savefig('./output/tree_high.png')
 
-# Save a dataframe to csv
-fp.to_csv('./output/question2_out_fpgrowth.csv', index=False, float_format='%g')
+print('Creating low threshold decision tree classifier ...')
+clf_low = DecisionTreeClassifier(criterion='entropy',
+                                 min_impurity_decrease=0.1)
+clf_low_model = clf_low.fit(training_data, test_data)
 
-# Produce association rules. I found that a threshold of 0.79 after,
-# repeated testing, produced exactly 10 rules.
-# The lowest being 0.790576 so that would be the exact threshold value
-# for 10 association rules
-assoc_rules_79_pc = association_rules(fp,
-                                      metric="confidence",
-                                      min_threshold=0.79)
-
-# Save a dataframe to csv
-assoc_rules_79_pc.to_csv('./output/question2_out_rules.csv', index=False, float_format='%g')
+# Set the figsize so we can clearly see the arrows between
+# each leaf in the decision tree
+plt.figure(figsize=(18, 8))
+plot_tree(clf_low_model, filled=True)
+plt.show()
+plt.tight_layout()
+plt.savefig('./output/tree_low.png')
 
 print('Done!')
